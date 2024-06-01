21b58060e165d3097c685fc03fc2a507e287e137
Rafael Vieira <rafaelralfi@gmail.com>
2024-05-31 21:41:34 -0300

    Criado componente de Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {Header} from "@/components/header";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt_br">
-      <body className={inter.className}>{children}</body>
+
+      <body className={inter.className}><Header>{children}</Header></body>
+
     </html>
   );
 }
