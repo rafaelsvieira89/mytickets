c96e7dfa5c8896a31a61e3ab6b4248f5b9f903a6
Rafael Vieira <rafaelralfi@gmail.com>
2024-06-01 18:13:40 -0300

    feat: Incluido botao de abrir chamado

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,19 +2,25 @@ import {Container} from "@/components/container";
 import {getServerSession} from "next-auth";
 import {authOptions} from "@/lib/auth";
 import {redirect} from "next/navigation";
+import Link from "next/link";
 
 export default async function Dashboard() {
     const session = await getServerSession(authOptions)
 
-    if(!session || !session.user){
+    if (!session || !session.user) {
         redirect("/")
     }
 
     return (
         <Container>
-            <div>
-                <h1>Dashboard</h1>
-            </div>
+            <main className="mt-9 mb-2">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-3xl font-bold">Chamados</h1>
+                    <Link href="/dashboard/new" className="bg-blue-500 text-white rounded px-4 py-1">
+                        Abrir Chamado
+                    </Link>
+                </div>
+            </main>
         </Container>
     )
 }
