7c45f891c4bea556e575b31898bd3c6546fd4d08
Rafael Vieira <rafaelralfi@gmail.com>
2024-06-01 18:07:10 -0300

    feat: criada pagina de clientes

---
+++ src/app/dashboard/customer/page.tsx
@@ -0,0 +1,11 @@
+import {Container} from "@/components/container";
+
+export default function Customer(){
+    return(
+        <Container>
+            <div>
+                Clientes
+            </div>
+        </Container>
+    )
+}
