d32885e9a342afd38d10be51824f047cd49a7c3e
Rafael Vieira <rafaelralfi@gmail.com>
2024-05-31 22:30:47 -0300

    Configurado next-auth e prisma ORM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,32 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
 import {Header} from "@/components/header";
+import {AuthProvider} from "@/providers/auth";
 
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "My Tickets - sistema de gerenciamento de tickets",
-  description: "Gerencie seus clientes e atendimentos facilmente",
+    title: "My Tickets - sistema de gerenciamento de tickets",
+    description: "Gerencie seus clientes e atendimentos facilmente",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt_br">
+    return (
+        <html lang="pt_br">
 
-      <body className={inter.className}><Header/>{children}</body>
+        <body className={inter.className}>
+        <AuthProvider>
+            <Header/>
+            {children}
+        </AuthProvider>
+        </body>
 
-    </html>
-  );
+        </html>
+    );
 }
