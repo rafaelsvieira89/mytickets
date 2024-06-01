0aa564cd9407e43b09a41f8fccd17dbef9a24c24
Rafael Vieira <rafaelralfi@gmail.com>
2024-06-01 19:20:37 -0300

    feat: protegida rota de novo cliente e criado componente de form para cadastro do cliente

---
+++ src/app/dashboard/customer/components/form/index.tsx
@@ -0,0 +1,9 @@
+"use client"
+export function NewCustomerForm(){
+    return(
+        <form>
+            <label>Nome completo</label>
+            <input type="text" placeholder="Digite nome completo"/>
+        </form>
+    )
+}
--- src/app/dashboard/customer/new/page.tsx
+++ src/app/dashboard/customer/new/page.tsx
@@ -1,9 +1,30 @@
 import {Container} from "@/components/container";
+import Link from "next/link";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/lib/auth";
+import {redirect} from "next/navigation";
+import {NewCustomerForm} from "@/app/dashboard/customer/components/form";
 
-export default function NewCustomer(){
-    return(
+export default async function NewCustomer() {
+
+    const session = await getServerSession(authOptions)
+
+    if(!session || !session.user)
+        redirect("/")
+
+    return (
         <Container>
-            <h1>Pagina cadastro de cliente</h1>
+            <main className="flex flex-col mt-9 mb-2">
+                <div className="flex items-center gap-3">
+                    <Link href="/dashboard/customer" className="bg-gray-900 px-4 py-1 rounded text-white">Voltar</Link>
+                    <h1 className="text-3xl font-bold">Novo cliente</h1>
+                </div>
+
+                <NewCustomerForm/>
+
+
+
+            </main>
         </Container>
     )
 }
