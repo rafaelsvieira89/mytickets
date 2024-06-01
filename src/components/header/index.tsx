9f62580a972386598e73b68cf4f2ca75b609079e
Rafael Vieira <rafaelralfi@gmail.com>
2024-06-01 17:40:45 -0300

    Ajustado comportamento de login, logout

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,20 @@
+"use client"
 import Link from "next/link";
-import {FiLogOut, FiUser} from "react-icons/fi";
+import {FiLoader, FiLock, FiLogOut, FiUser} from "react-icons/fi";
+import {signIn, signOut, useSession} from "next-auth/react";
 
 export function Header() {
+
+    const {status, data} = useSession()
+
+    async function handleLogin() {
+        await signIn()
+    }
+
+    async function handleLogout() {
+        await signOut()
+    }
+
     return (
         <header className="w-full flex items-center px-2 bg-white h-20 shadow-sm">
             <div className="w-full flex items-center justify-between max-w-7xl mx-auto">
@@ -9,17 +22,41 @@ export function Header() {
                     <h1 className="font-bold text-2xl pl-1 hover:tracking-widest duration-300">
                         <span className="text-blue-500">My</span> Tickets</h1>
                 </Link>
-                <div className="flex items-baseline gap-4">
-                    <Link href="/dashboard">
-                        <FiUser size={26} color="#4b5563"/>
-                    </Link>
 
-                    <button>
 
-                        <FiLogOut size={26} color="#4b5563"/>
+                {
+                    status === "loading" && (
+                        <button className="animate-spin">
+                            <FiLoader size={26}/>
+                        </button>
+                    )
+                }
 
-                    </button>
-                </div>
+                {
+                    status === "unauthenticated" && (
+                        <button onClick={handleLogin}>
+                            <FiLock size={26}/>
+                        </button>
+                    )
+                }
+
+                {
+                    status === "authenticated" && (
+                        <div className="flex items-baseline gap-4">
+                            <Link href="/dashboard">
+                                <FiUser size={26} color="#4b5563"/>
+                            </Link>
+
+                            <button onClick={handleLogout}>
+
+                                <FiLogOut size={26} color="#4b5563"/>
+
+                            </button>
+                        </div>
+                    )
+                }
+
+
             </div>
         </header>)
 }
