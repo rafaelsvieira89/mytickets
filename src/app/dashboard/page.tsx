c883ae63b014fc1e0456f328c1389237f44b7948
Rafael Vieira <rafaelralfi@gmail.com>
2024-05-31 21:56:06 -0300

    Criada pagina de dashboard e criado container para desacoplar configuracoes de layout respeitando tamanho do header

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import {Container} from "@/components/container";
+
+export default function Dashboard() {
+    return (
+        <Container>
+            <div>
+                <h1>Dashboard</h1>
+            </div>
+        </Container>
+    )
+}
