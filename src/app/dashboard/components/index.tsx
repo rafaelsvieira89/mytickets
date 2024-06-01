b6ef2939f82a0d3e25d585996a9ff4fc67e7e8a8
Rafael Vieira <rafaelralfi@gmail.com>
2024-06-01 18:06:11 -0300

    style: componente de dashboard estilizado

--- src/app/dashboard/components/index.tsx
+++ src/app/dashboard/components/index.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 export function DashboardHeader(){
     return (
         <Container>
-            <header>
-                <Link href="/dashboard">
+            <header className="w-full bg-gray-900 my4 p-3 rounded flex gap-4 items-center">
+                <Link href="/dashboard" className="text-white hover:font-bold duration-300">
                     Chamados
                 </Link>
-                <Link href="/dashboard/customer">
+                <Link href="/dashboard/customer" className="text-white hover:font-bold duration-300">
                     Clientes
                 </Link>
             </header>
