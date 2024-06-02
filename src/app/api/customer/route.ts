fa52ae422690dfaed9e8be5b782b529294c7fa0d
Rafael Vieira <rafaelralfi@gmail.com>
2024-06-01 21:41:03 -0300

    feat: Criada rota de backend para criar cliente utilizando o prisma. Protegida rota para ter autenticação

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -1,9 +1,31 @@
 import {NextResponse} from "next/server";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/lib/auth";
+import prisma from "@/lib/prisma";
 
-export async function POST(request: Request){
+export async function POST(request: Request) {
 
-    const {name, phone, email, address} = await request.json()
+    const session = await getServerSession(authOptions)
+    if (!session || !session.user)
+        return NextResponse.json({error: "Não autorizado"}, {status: 401})
 
 
-    return NextResponse.json({message: "Rota de cadastro"})
+    const {name, phone, email, address, userId} = await request.json()
+
+    try {
+        await prisma.customer.create({
+            data: {
+                name,
+                phone,
+                email,
+                address: address ? address : "",
+                userId: userId
+            }
+        })
+        return NextResponse.json({message: "Cliente cadastrado com sucesso"})
+    } catch (err) {
+        return NextResponse.json({error: "Não foi possível criar um novo cliente"}, {status: 400})
+    }
+
+
 }
