6137d12f0ceaedde6b81a60adaadbc4cea0d88d8
Rafael Vieira <rafaelralfi@gmail.com>
2024-06-04 12:13:18 -0300

    bugfix: Correção para funcionar o tipo de dados user com id

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,12 @@
 import {PrismaAdapter} from "@auth/prisma-adapter"
+import type {Adapter} from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google"
 import {AuthOptions} from "next-auth";
 import prismaClient from "@/lib/prisma";
 
 
 export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(prismaClient),
+    adapter: PrismaAdapter(prismaClient) as Adapter,
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
