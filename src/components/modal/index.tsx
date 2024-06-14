00250b62133a3e8312f893209d334e646a3e0404
Rafael Vieira <rafaelralfi@gmail.com>
2024-06-14 17:53:56 -0300

    feat: Criado titulo do modal e botao de fechar

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -6,7 +6,11 @@ export function ModalTicket() {
             <div className="absolute inset-0 flex items-center justify-center">
                 <div id="centraliza-conteudo-absoluto"
                      className="bg-white shadow-lg w-4/5 md:w-1/2 max-w-2xl p-3 rounded">
-                    <h1>Conteudo modal</h1>
+                    <div className="flex items-center justify-between">
+                    <h1 className="font-bold text-lg md:text-2xl">Detalhes do chamado</h1>
+                        <button className="bg-red-500 rounded p-1 px-2">Fechar</button>
+                    </div>
+
                 </div>
             </div>
         </section>
