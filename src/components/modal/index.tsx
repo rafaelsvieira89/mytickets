ef338c0129272039958c323411fd5f8d59269993
Rafael Vieira <rafaelralfi@gmail.com>
2024-06-14 18:13:00 -0300

    feat: Ação de fechar modal com click fora do modal e no botão de fechar

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,14 +1,28 @@
 "use client"
 
+import {useContext, useRef, MouseEvent} from "react";
+import {ModalContext} from "@/providers/modal";
+
 export function ModalTicket() {
+
+    const {handleModalVisible} = useContext(ModalContext)
+    const modalRef = useRef<HTMLDivElement | null>(null);
+
+    function handleCloseModal(e: MouseEvent<HTMLDivElement>) {
+        if(modalRef.current && !modalRef.current.contains(e.target as Node)){
+            handleModalVisible()
+        }
+    }
+
+
     return (
-        <section className="absolute bg-gray-900/80 w-full min-h-screen">
+        <div className="absolute bg-gray-900/80 w-full min-h-screen" onClick={handleCloseModal}>
             <div className="absolute inset-0 flex items-center justify-center">
-                <div id="centraliza-conteudo-absoluto"
+                <div ref={modalRef} id="centraliza-conteudo-absoluto"
                      className="bg-white shadow-lg w-4/5 md:w-1/2 max-w-2xl p-3 rounded">
                     <div className="flex items-center justify-between">
                         <h1 className="font-bold text-lg md:text-2xl">Detalhes do chamado</h1>
-                        <button className="bg-red-500 rounded p-1 px-2">Fechar</button>
+                        <button className="bg-red-500 rounded p-1 px-2" onClick={handleModalVisible}>Fechar</button>
                     </div>
 
                     <div className="flex gap-1 flex-wrap mb-2">
@@ -41,6 +55,6 @@ export function ModalTicket() {
 
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
