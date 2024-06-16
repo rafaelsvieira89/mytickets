86746e3951819cb2b9e695a22842359f58ba1241
Rafael Vieira <rafaelralfi@gmail.com>
2024-06-16 12:16:10 -0300

    feat: componente para exibição do cliente selecionado, com funcionalidade de limpar o cliente selecionado e voltar o state do cliente para nulo

--- src/app/open/page.tsx
+++ src/app/open/page.tsx
@@ -1,11 +1,10 @@
 "use client"
-import {Container} from "@/components/container";
 import {Input} from "@/components/input";
 import {z} from "zod"
 import {zodResolver} from "@hookform/resolvers/zod";
 import {useForm} from "react-hook-form";
-import {FiSearch} from "react-icons/fi";
-import {white} from "next/dist/lib/picocolors";
+import {FiSearch, FiX} from "react-icons/fi";
+import {useState} from "react";
 
 const schema = z.object({
     email: z.string().email("Digite o email do cliente para localizar").min(1, "O campo email é obrigatorio")
@@ -13,28 +12,54 @@ const schema = z.object({
 
 type FormData = z.infer<typeof schema>
 
+interface CustomerDataInfo {
+    id: string;
+    name: string;
+}
+
 export default function OpenTicket() {
-    const {register, formState: {errors}} = useForm<FormData>({
+
+    const [customer, setCustomer] = useState<CustomerDataInfo | null>(null)
+
+    const {
+        register,
+        setValue,
+        formState: {errors}
+    } = useForm<FormData>({
         resolver: zodResolver(schema),
     })
+
+    function handleClearCustomer() {
+        setCustomer(null)
+        setValue("email", "")
+    }
 
     return (
         <div className="w-full max-w-2xl mx-auto px-2">
             <h1 className="font-bold text-3xl text-center mt-24">Abrir Chamado</h1>
             <main className="flex flex-col mt-4 mb-2">
-                <form className="bg-slate-200 py-6 px-2 rounded border-2">
-                    <div className="flex flex-col gap-3">
-                        <Input type="text" placeholder="Informe o email do cliente..." name="email" register={register}
-                               error={errors.email?.message}/>
-                        <button
-                            className="flex bg-blue-500 flex-row gap-3 px-2 h-11
-                                text-white items-center justify-center rounded active:bg-blue-400 font-bold">
-                            Procurar cliente
-                            <FiSearch size={24} color="#FFF"/>
+                {customer ? (
+                    <div className="bg-slate-200 py-6 px-4 rounded border-2 flex justify-between items-center">
+                        <p className="text-lg"><strong>Cliente selecionado:</strong> {customer.name}</p>
+                        <button className="h-11 px-2 rounded flex items-center justify-center" onClick={handleClearCustomer}>
+                            <FiX size={30} color="#ff2929"/>
                         </button>
-                    </div>
+                    </div>) : (
+                    <form className="bg-slate-200 py-6 px-2 rounded border-2">
+                        <div className="flex flex-col gap-3">
+                            <Input type="text" placeholder="Informe o email do cliente..." name="email"
+                                   register={register}
+                                   error={errors.email?.message}/>
+                            <button
+                                className="flex bg-blue-500 flex-row gap-3 px-2 h-11
+                                text-white items-center justify-center rounded active:bg-blue-400 font-bold">
+                                Procurar cliente
+                                <FiSearch size={24} color="#FFF"/>
+                            </button>
+                        </div>
 
-                </form>
+                    </form>
+                )}
             </main>
         </div>
     )
